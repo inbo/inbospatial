ccfb7781c3af3631253876af555ca2335dd11e9b
Falk Mielke <falk.mielke@inbo.be>
2024-10-18 12:13:50 +0200

    DHMV API: rebase, mht adjustment, vignette*
    + rebased to follow the latest dev by @hansvancalster on parallel
    branches
    + incorporated review suggestions
    + modified `unpack_mht()`: the function will now return the tif path
    + additional vignette paragraph: using DHMV `v2.0.1`

--- R/get_coverage_wcs.R
+++ R/get_coverage_wcs.R
@@ -67,8 +67,16 @@ get_coverage_wcs <- function(
 
   # constrain version | wcs
   if (wcs == "dhmv") {
-    assert_that(version == "1.0.0",
-      msg = "WCS `DHMV` is incompatible with versions other than `1.0.0`.")
+    # warn incompatible versions
+    if (!(version %in% c("1.0.0", "2.0.1")) {
+      message("WCS `DHMV` is only compatible with versions `1.0.0` or `2.0.1`.
+        Consider using `version=\"2.0.1\"`")
+    }
+    # recommend crs specification
+    if (wcs_crs != "EPSG:31370") {
+      message("WCS `DHMV` only supports CRS Belgian Lambert 72 (`EPSG:31370`).
+        Consider specifying `wcs_csr=\"EPSG:31370\"`")
+    }
   }
 
   # set url
@@ -78,14 +86,6 @@ get_coverage_wcs <- function(
     dtm = "https://geo.api.vlaanderen.be/el-dtm/wcs",
     dsm = "https://geo.api.vlaanderen.be/el-dsm/wcs",
     dhmv = "https://geo.api.vlaanderen.be/DHMV/wcs"
-  )
-
-  # assure package availability
-  stopifnot(
-    assertthat = require("assertthat", quietly = TRUE),
-    httr = require("httr", quietly = TRUE),
-    sf = require("sf", quietly = TRUE),
-    terra = require("terra", quietly = TRUE)
   )
 
   # data type assertions
@@ -140,15 +140,14 @@ get_coverage_wcs <- function(
 
     # build and run the http request
     request <- build_url(url)
-    file <- tempfile(fileext = ".mht")
+    mht_file <- tempfile(fileext = ".mht")
     http_response <- GET(
       url = request,
-      write_disk(file)
+      write_disk(mht_file)
     )
 
     # multipart file extract tif part
-    unpack_mht(file)
-    file <- str_replace(file, "mht", "tif")
+    tif_file <- unpack_mht(mht_file)
   } # /version 2.0.1
 
 
@@ -176,10 +175,10 @@ get_coverage_wcs <- function(
 
     # build and run the http request
     request <- build_url(url)
-    file <- tempfile(fileext = ".tif")
+    tif_file <- tempfile(fileext = ".tif")
     http_response <- GET(
       url = request,
-      write_disk(file)
+      write_disk(tif_file)
     )
   }
 
@@ -187,7 +186,7 @@ get_coverage_wcs <- function(
   stop_for_status(http_response)
 
   # assemble the spatial raster
-  raster <- rast(file)
+  raster <- rast(tif_file)
   template <- project(raster, output_crs)
   res(template) <- resolution
   raster <- project(raster, template)
@@ -196,16 +195,18 @@ get_coverage_wcs <- function(
 }
 
 
-#' Unpack or extract the `tif` file part from an `mht` file
+#' Unpack or extract the `tif` file part from an `mht` file.
 #'
 #' This helper function is needed on some `WCS` services from which an `mht`
-#' file is downloaded rather than a `tif` file
+#' file is downloaded rather than a `tif` file; returns the path to the `tif`
 #'
 #' @param path A path to the `mht` file
 #'
 #' @importFrom readr read_lines_raw read_lines read_file_raw write_file
 #' @importFrom assertthat assert_that
 #' @importFrom stringr str_detect str_replace
+#'
+#' @return tif_path the path to the extracted geoTIFF.
 #'
 #' @keywords internal
 #'
@@ -228,8 +229,10 @@ unpack_mht <- function(path) {
   pos_end <- length(raw_vector) - (length(lines_raw[end + 1]) + 1)
 
   tif <- raw_vector[pos_start:pos_end]
+  tif_path <- str_replace(path, "mht", "tif")
   write_file(
     tif,
-    str_replace(path, "mht", "tif")
+    tif_path
   )
+  return(tif_path)
 }
