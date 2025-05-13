8f6c886520801fed002c7f4e4edc22032a7a07f9
Falk Mielke <falk.mielke@inbo.be>
2025-05-13 13:36:45 +0200

    silence_unpack_mht: url warning (checklist)

--- R/get_feature_wfs.R
+++ R/get_feature_wfs.R
@@ -6,7 +6,7 @@
 #' The request is made up of key-value pairs and additional key-value pairs can
 #' be passed to the function.
 #' The full documentation for the `WFS` standard can be consulted from
-#' \url{https://www.ogc.org/publications/standard/wfs/}.
+#' \url{https://www.ogc.org/standard/wfs/}.
 #'
 #' @param wfs Web address for the service which you want to query features from
 #' @param version Version number for the service.
@@ -18,7 +18,7 @@
 #' Pass this as a named vector with names `"xmin"`, `"xmax"`, `"ymin"`,
 #' `"ymax"`.
 #' @param filter Optional
-#' [standard OGC filter](https://www.ogc.org/publications/standard/filter/)
+#' [standard OGC filter](https://www.ogc.org/standard/filter/)
 #' specification
 #' @param cql_filter Optional
 #' [Contextual Query Language](https://portal.ogc.org/files/96288) filter.
